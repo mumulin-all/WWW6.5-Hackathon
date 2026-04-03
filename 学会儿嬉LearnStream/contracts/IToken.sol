a2ba7e92620ab8a42f1428c3452fbf67d8154e92
foreinsneun9 <winterreeeeeeee@gmail.com>
2026-04-03 13:54:10 +0000

    delete old files

--- 学会儿嬉LearnStream/contracts/IToken.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
-
-interface IToken {
-    function mint(address to, uint256 amount) external;
-    function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 amount) external returns (bool);
-}
